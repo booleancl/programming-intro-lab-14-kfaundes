ca157552c43cf4dac04b106548e9d29bd5833dfe
unknown <kfaundesmadrid@gmail.com.com>
2022-10-18 23:14:15 -0300

    dat

--- migrations/4_seedData.sql
+++ migrations/4_seedData.sql
@@ -1,1 +1,5 @@
+\c blog
 
+\COPY users(email) FROM C:/Users/kfaun/intro_dev/programming-intro-lab-14-kfaundes/data/users.csv HEADER;
+\COPY post FROM C:/Users/kfaun/intro_dev/programming-intro-lab-14-kfaundes/data/posts.csv HEADER,
+\COPY comments FROM C:/Users/kfaun/intro_dev/programming-intro-lab-14-kfaundes/data/comentarios.csv HEADER;
--- queries/5_posts-with-users.sql
+++ queries/5_posts-with-users.sql
@@ -1,1 +1,9 @@
 /* Seleccionar el correo del usuario, el id del post y el título de los posts */
+
+\c blog 
+
+SELECT email, posts.id, title
+FROM users
+JOIN posts ON posts.users_id = users_id
+WHERE posts.user_id IS NULL;
+
